5efaec7ace76e098dccb24b369cae5fa11e4818e
Adam Kendis <atkendis@gmail.com>
2018-10-30 16:14:41 -0700

    Implemented server route for specific listing id. Database queried for listing id upon GET request.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment];
+const knex = require('knex')(config);
 let app = express();
 let port = 3000;
 
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 
-app.get('/listing-details', (req, res) => {
-  console.log('test GET request');
-  res.send();
+app.get('/listing-details/:listingId', (req, res) => {
+  var listingId = req.params.listingId;
+  knex('listings')
+    .where({id: listingId})
+    .then(listing => (console.log(listing)))
+    res.send()
 })
 
 app.listen(port, () => {
